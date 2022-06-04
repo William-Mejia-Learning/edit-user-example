bb9cd35527cd41f07f739c4e9ff60f48ca17507c
William Mejia <willx1995@gmail.com>
2022-06-04 13:48:31 -0500

    feat:example edit user

---
+++ src/main/java/com/example/userproject/controllers/UserController.java
@@ -0,0 +1,71 @@
+package com.example.userproject.controllers;
+
+import com.example.userproject.models.User;
+import com.example.userproject.repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.persistence.PreUpdate;
+
+@Controller
+public class UserController {
+    private final UserRepository userDao;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserController(UserRepository userDao, PasswordEncoder passwordEncoder) {
+        this.userDao = userDao;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/profile")
+    public String showProfile(){
+        return "profile";
+    }
+
+    @GetMapping("/")
+    public String signIn(Model model){
+        model.addAttribute("user", new User());
+
+        return "sign-in";
+
+    }
+
+    @PostMapping("/")
+    public String createUser(@ModelAttribute User user){
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        userDao.save(user);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String showLogIng(){
+        return "login";
+    }
+
+    @GetMapping("/edit")
+    public String editForm(Model model){
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("useredit", userDao.findById(user.getId()));
+        return "/edit-profile";
+    }
+
+    @PostMapping("/edit")
+    public String submitEdit(@ModelAttribute User user){
+        User userInfoPull = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        User editUser = userDao.getById(userInfoPull.getId());
+
+        editUser.setUsername(user.getUsername());
+        editUser.setEmail(user.getEmail());
+
+        userDao.save(editUser);
+        return "redirect:/profile";
+    }
+}
---
+++ src/main/java/com/example/userproject/models/User.java
@@ -0,0 +1,90 @@
+package com.example.userproject.models;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    private String location;
+
+    public User(String username, String email, String password, String location) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.location = location;
+    }
+
+    public User(User copy) {
+        id = copy.id; // This line is SUPER important! Many things won't work if it's absent
+        email = copy.email;
+        username = copy.username;
+        password = copy.password;
+        location = copy.location;
+
+    }
+
+    public User() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
